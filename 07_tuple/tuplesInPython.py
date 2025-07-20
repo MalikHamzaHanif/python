c661ac58aae7934a59a15a4faa9d7e513b0ea102
Hamza Hanif <www.musamalik9@gmail.com>
2025-07-21 04:00:20 +0500

    tuple in python

---
+++ 07_tuple/tuplesInPython.py
@@ -0,0 +1,14 @@
+tupleOne=("hamza","ali","rehan")
+print(type(tupleOne))
+
+print(tuple)
+print(tuple[1])
+
+if "hamza" in tupleOne:
+    print("haan hy bhaiiii")
+
+
+colors=("red","blue","green")
+
+(red,blue,greeeen)=colors
+print(greeeen)
