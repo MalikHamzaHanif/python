d64d2fc9c114c43d0fb465976d2af6c3cf9935ed
Hamza Hanif <www.musamalik9@gmail.com>
2025-07-21 20:46:11 +0500

    loops in python

---
+++ 09_loops/loopsInPython.py
@@ -0,0 +1,58 @@
+# positiveNumbers=0
+# list=[1,2,3,4,-1,-3,-2,12]
+
+# for num in list:
+#     if num>0:
+#         positiveNumbers+=1
+# print("no of positive numbers",positiveNumbers)
+
+# sum=0
+# upTo=9
+
+# for num in range(upTo):
+#     if num%2==0:
+#         sum+=num
+
+# print("sum is ",sum)
+
+
+# number=3
+
+# for num in range (1,11):
+#     if num==5:
+#         continue
+#     print("{} * {} = {}".format(number,num,num*number))
+
+
+# sentence="i am just a sentence"
+# reverseStr=""
+# totalLength=len(sentence)
+# for i in range(len(sentence)):
+#     reverseStr+=sentence[totalLength-1]
+#     totalLength-=1
+# print(reverseStr)
+
+# for char in sentence:
+#     if(sentence.count(char)==1):
+#         print("{} is only 1 time in sentence".format(char))
+#         break
+
+# while True:
+#     print("enter values")
+#     value=int(input())
+#     if value>=0 and value<=10:
+#         print("good")
+#         break
+
+# import time
+
+# noOfAttempt=0
+# maxAttempts=5
+# wait=2
+
+# while noOfAttempt<maxAttempts:
+#     print("please try again after ",wait)
+#     wait*=2
+#     noOfAttempt+=1
+#     time.sleep(wait)
+    
